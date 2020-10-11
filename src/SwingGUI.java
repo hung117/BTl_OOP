9df239c731134344f9440f33101cb919b99ded7c
emptywhite1 <emptywhite2@gmail.com>
2020-10-11 23:47:38 +0700

    reorganize

---
+++ src/SwingGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class SwingGUI {
+    private JButton addWordButton;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+}
